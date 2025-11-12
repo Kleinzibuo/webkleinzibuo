307d3fbf46fe1168c66b733eacea586901b273af
Thembinkosi Klein <thembinkosiklein@users.noreply.github.com>
2025-11-12 21:45:05 +0200

    boilerplate

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Welcome to KleinZibuo Website</h1>
-      <p>Our preschool website  is coming soon!</p>
-    </main>
-  );
-}
