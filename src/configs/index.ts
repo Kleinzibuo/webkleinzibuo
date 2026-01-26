cb15456466037bd6621a5aad1768fea4255348f3
Thembinkosi Klein <thembinkosiklein@users.noreply.github.com>
2026-01-26 19:47:43 +0200

    Update index.ts

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,4 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "https://web.api.kleinzibuo.co.za/graphql";
+export const API_URL = "https://web.api.kleinzibuo.co.za/graphql";
 
 // export const API_URL =
 //   process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/graphql";
